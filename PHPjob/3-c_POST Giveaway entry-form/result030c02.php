7dcff1db36f55964cd66f70906d9bc4dca7aa401
cap3141592 <121149436+cap3141592@users.noreply.github.com>
2023-01-02 10:45:04 +0900

    3章チャレンジ（フォーム POST通信）
    景品応募フォームの作成
    入力内容に応じたinputタグなどの選択

---
+++ PHPjob/3-c_POST Giveaway entry-form/result030c02.php
@@ -0,0 +1,11 @@
+<?php
+
+$c_name = $_POST['custom_name'];
+$rank = $_POST['rank'];
+$purchnum = $_POST['purch_num'];
+
+?>
+
+<p>お名前：<?php echo $c_name; ?></p>
+<p>ご希望商品：<?php echo $rank; ?></p>
+<p>個数：<?php echo $purchnum; ?></p>
